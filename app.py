d5acf02dcb9830777cd38b204172dc45c818be3f
Francis Osei <francis@bayezian.com>
2024-02-25 17:38:44 +0000

    files

--- app.py
+++ app.py
@@ -2,15 +2,23 @@ import streamlit as st
 from streamlit_chat import message
 from langchain.chains import ConversationalRetrievalChain
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.llms import LlamaCpp
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import CTransformers
+from langchain.llms import Replicate
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
 from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import Docx2txtLoader
+from langchain_community.embeddings import HuggingFaceHubEmbeddings
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 import os
+import replicate
+from dotenv import load_dotenv
 import tempfile
 
 
+load_dotenv()
 
 
 def initialize_session_state():
@@ -34,10 +42,11 @@ def display_chat_history(chain):
 
     with container:
         with st.form(key='my_form', clear_on_submit=True):
-            user_input = st.chat_input("Message ACHIAAAI")
+            user_input = st.text_input("Question:", placeholder="Ask about your Documents", key='input')
+            submit_button = st.form_submit_button(label='Send')
 
-        if user_input:
-            with st.spinner('Processing'):
+        if submit_button and user_input:
+            with st.spinner('Generating response...'):
                 output = conversation_chat(user_input, chain, st.session_state['history'])
 
             st.session_state['past'].append(user_input)
@@ -50,16 +59,17 @@ def display_chat_history(chain):
                 message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
 
 def create_conversational_chain(vector_store):
+    load_dotenv()
     # Create llm
-    llm = LlamaCpp(
-    streaming = True,
-    model_path="mistral-7b-instruct-v0.1.Q4_0.gguf",
-    temperature=0.75,
-    top_p=1, 
-    verbose=True,
-    n_ctx=4096
-)
-    
+    #llm = CTransformers(model="llama-2-7b-chat.ggmlv3.q4_0.bin",
+                        #streaming=True, 
+                        #callbacks=[StreamingStdOutCallbackHandler()],
+                        #model_type="llama", config={'max_new_tokens': 500, 'temperature': 0.01})
+    llm = Replicate(
+        streaming = True,
+        model = "replicate/llama-2-70b-chat:58d078176e02c219e11eb4da5a02a7830a283b14cf8f94537af893ccff5ee781", 
+        callbacks=[StreamingStdOutCallbackHandler()],
+        input = {"temperature": 0.01, "max_length" :500,"top_p":1})
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
     chain = ConversationalRetrievalChain.from_llm(llm=llm, chain_type='stuff',
@@ -68,10 +78,13 @@ def create_conversational_chain(vector_store):
     return chain
 
 def main():
+    load_dotenv()
     # Initialize session state
     initialize_session_state()
+    st.title("Multi-Docs ChatBot using llama2 :books:")
     # Initialize Streamlit
-    uploaded_files = st.sidebar.file_uploader("Upload documents", accept_multiple_files=True)
+    st.sidebar.title("Document Processing")
+    uploaded_files = st.sidebar.file_uploader("Upload files", accept_multiple_files=True)
 
 
     if uploaded_files:
@@ -85,17 +98,20 @@ def main():
             loader = None
             if file_extension == ".pdf":
                 loader = PyPDFLoader(temp_file_path)
+            elif file_extension == ".docx" or file_extension == ".doc":
+                loader = Docx2txtLoader(temp_file_path)
+            elif file_extension == ".txt":
+                loader = TextLoader(temp_file_path)
 
             if loader:
                 text.extend(loader.load())
                 os.remove(temp_file_path)
 
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+        text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=100, length_function=len)
         text_chunks = text_splitter.split_documents(text)
 
         # Create embeddings
-        embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", 
-                                           model_kwargs={'device': 'cpu'})
+        embeddings = HuggingFaceHubEmbeddings()
 
         # Create vector store
         vector_store = FAISS.from_documents(text_chunks, embedding=embeddings)
