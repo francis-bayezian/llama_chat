fa6d768ee94577e7cfbcd4150167dee7487b77d7
Francis Osei <francis@bayezian.com>
2024-02-25 18:18:41 +0000

    files

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def main():
     load_dotenv()
     # Initialize session state
     initialize_session_state()
-    st.title("Multi-Docs ChatBot using OpenSource LLM :books:")
+    st.title("OpenSource LLM ChatBot :books:")
     # Initialize Streamlit
     st.sidebar.title("Document Processing")
     uploaded_files = st.sidebar.file_uploader("Upload files", accept_multiple_files=True)
